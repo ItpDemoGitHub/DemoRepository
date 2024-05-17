e937ea3383e76b09d27eddda086546865c9a3092
Yogesh Nagar <yogesh61189@gmail.com>
2024-05-17 11:14:17 +0530

    Added new programe in project

---
+++ src/main/java/org/example/TestSequence.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class TestSequence {
+    public static void main(String[] args) {
+        String str = "I love java";
+        for(int index=0;index<str.length();index++){
+            System.out.println(str.charAt(index));
+        }
+    }
+}
