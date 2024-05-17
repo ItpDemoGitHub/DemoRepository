1080d7a42606b3907ee2f3e316cd1eced9256acf
ItpDemoGitHub <itpa4tech2024@gmail.com>
2024-05-17 11:15:08 +0530

    Update TestSequence.java

--- src/main/java/org/example/TestSequence.java
+++ src/main/java/org/example/TestSequence.java
@@ -4,7 +4,7 @@ public class TestSequence {
     public static void main(String[] args) {
         String str = "I love java";
         for(int index=0;index<str.length();index++){
-            System.out.println(str.charAt(index));
+            System.out.println(index+" --> "+str.charAt(index));
         }
     }
 }
